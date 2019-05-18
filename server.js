50532552b346036a89b26cc6dbaac3eea15d1e3a
Aremu Benjamen <bendayo38@gmail.com>
2019-05-18 16:04:55 -0700

    fixed count error

--- server.js
+++ server.js
@@ -134,20 +134,17 @@ app.get('/profile/:id', (req, res) =>{
 })
 // image with put method-for image update
 app.put('/image', (req,res) =>{
-    const {id} = req.body;
-    let found = false;
-    database.users.forEach(user =>{
-        if(user.id === id){
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    const { id } =req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries =>{
+        res.json(entries[0])
     })
-    if(!found){
-        res.status(400).json('not found');
-    }
-})
-//bcrypt
+    .catch(err => res.status(400).json('unable to get entries'))
+    })
+        
+
 
 
 app.listen(3000, ()=>{
